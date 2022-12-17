8b84e31f8eecaa2ac789660581e3577c2442d29e
LeonardoMunhoz770 <munhozleonardo756@gmail.com>
2022-12-17 18:23:58 -0300

    Sistema de Login: Firebase

--- src/loginPage/script.js
+++ src/loginPage/script.js
@@ -1,9 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "../../node_modules/firebase/app";
-import { getAnalytics } from "../../node_modules/firebase/analytics";
-import {getAuth} from '../../node_modules/firebase/auth'
-import {getFirestore} from '../../node_modules/firebase/firestore'
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+import { getAuth , signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,18 +21,30 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app)
 
 
-const auth = firebase.auth();
-const provider = new firebase.auth.GoogleAuthProvider();
-setTimeout(function(){
-  auth.signInWithPopup(provider)
-}, 3000)
+document.querySelector("#signIn").addEventListener("click", function(){
+  let email = document.querySelector("#email").value;
+  let password = document.querySelector("#password").value;
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
 
-
-auth.onAuthStateChanged((val) =>{
-  if(val){
-    alert('logado com sucesso')
+    // ...
     
-  }
+    console.log('user Logado')
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Algo deu errado',
+      footer: `<label>Contate o administrador! ${errorMessage}</label>`,
+      timer: 2000
+    })
+  });
 })
